a8a9f8ebfeef73145d68e78f36081241a1565129
N1kk1337 <nikillerok@gmail.com>
2024-10-23 20:40:57 +0300

    fix: changed all imports to aliases

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { Provider } from 'react-redux';
-import { store } from './app/store/store';
+import { store } from '~/app/store/store';
 
-import './index.css'
-import './App.css'
+import '~/index.css'
+import '~/App.css'
 import App from '~/pages/main'
 
 
