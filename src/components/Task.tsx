736eea87f78bc4dca3fe61cda087b50cb787b902
AsukaTundra <emgushovs@mail.ru>
2024-10-25 16:45:18 +0300

    add main page styles and add new Tasks title

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,23 +1,23 @@
-import type React from "react";
-import { Box, Typography, IconButton } from "@mui/material";
-import CheckIcon from "@mui/icons-material/Check";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-import { useDispatch } from "react-redux";
-import type { AppDispatch } from "~/app/store/store";
-import { deleteTodo, fetchTodos, updateTodo } from "~/app/store/todosSlice";
-import type { Todo } from "~/shared/types/todoTypes";
-
+import type React from 'react'
+import { Box, Typography, IconButton } from '@mui/material'
+import CheckIcon from '@mui/icons-material/Check'
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline'
+import { useDispatch } from 'react-redux'
+import type { AppDispatch } from '~/app/store/store'
+import { deleteTodo, fetchTodos, updateTodo } from '~/app/store/todosSlice'
+import type { Todo } from '~/shared/types/todoTypes'
+import st from '~/shared/styles/task.module.scss'
 const Task: React.FC<Todo> = ({ title, id, userId, completed }) => {
-	const dispatch = useDispatch<AppDispatch>();
+	const dispatch = useDispatch<AppDispatch>()
 
 	const handleDeleteTodo = (id: string) => {
-		console.log(id);
+		console.log(id)
 		dispatch(deleteTodo(id))
 			.then(() => {
-				dispatch(fetchTodos());
+				dispatch(fetchTodos())
 			})
-			.catch((er) => console.log(er));
-	};
+			.catch(er => console.log(er))
+	}
 
 	const handleUpdateTodo = () => {
 		dispatch(
@@ -25,45 +25,53 @@ const Task: React.FC<Todo> = ({ title, id, userId, completed }) => {
 				id: id,
 				title: title,
 				completed: !completed,
-				userId: userId,
-			}),
+				userId: userId
+			})
 		).then(() => {
-			dispatch(fetchTodos());
-		});
-	};
+			dispatch(fetchTodos())
+		})
+	}
 	return (
 		<Box
 			sx={{
-				display: "flex",
-				alignItems: "center",
-				justifyContent: "space-between",
-				padding: "12px",
-				borderRadius: "8px",
-				marginBottom: "8px",
-				backgroundColor: "#15101C",
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'space-between',
+				padding: '12px',
+				borderRadius: '8px',
+				marginBottom: '8px',
+				backgroundColor: '#15101C'
 			}}
 		>
-			<Typography variant="body1" sx={{ color: "#9E78CF" }}>
+			<Typography
+				variant='body1'
+				sx={{ color: completed ? '#78CFB0 ' : '#9E78CF' }}
+				className={completed ? st.task : ''}
+			>
 				{title}
 			</Typography>
 			<Box>
+				{completed === true ? (
+					<></>
+				) : (
+					<IconButton
+						onFocus={e => e.currentTarget.blur()}
+						onClick={() => handleUpdateTodo()}
+						sx={{ color: '#9E78CF' }}
+					>
+						<CheckIcon />
+					</IconButton>
+				)}
 				<IconButton
-					onFocus={(e) => e.currentTarget.blur()}
-					onClick={() => handleUpdateTodo()}
-					sx={{ color: "#9E78CF" }}
-				>
-					<CheckIcon />
-				</IconButton>
-				<IconButton
-					onFocus={(e) => e.currentTarget.blur()}
+					onFocus={e => e.currentTarget.blur()}
 					onClick={() => handleDeleteTodo(id)}
-					sx={{ color: "#9E78CF" }}
+					sx={{ color: '#9E78CF' }}
 				>
 					<DeleteOutlineIcon />
 				</IconButton>
 			</Box>
 		</Box>
-	);
-};
+	)
+}
 
-export default Task;
+export default Task
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,57 +1,90 @@
-import type React from "react";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import type { AppDispatch, RootState } from "~/app/store/store";
+import type React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import type { AppDispatch, RootState } from '~/app/store/store'
 import {
 	selectTodosStatus,
 	selectTodosError,
-	fetchTodos,
-} from "~/app/store/todosSlice";
-import TaskInput from "~/components/TaskInput";
-import Task from "~/components/Task";
-import st from "~/shared/styles/main.module.scss";
+	fetchTodos
+} from '~/app/store/todosSlice'
+import TaskInput from '~/components/TaskInput'
+import Task from '~/components/Task'
+import st from '~/shared/styles/main.module.scss'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
+import { Typography } from '@mui/material'
 const MainPage: React.FC = () => {
-	const dispatch = useDispatch<AppDispatch>();
-	const todos = useSelector((state: RootState) => state.todos.items);
-	const status = useSelector(selectTodosStatus);
-	const error = useSelector(selectTodosError);
-
+	const dispatch = useDispatch<AppDispatch>()
+	const todos = useSelector((state: RootState) => state.todos.items)
+	const status = useSelector(selectTodosStatus)
+	const error = useSelector(selectTodosError)
+	const [parent] = useAutoAnimate({ easing: 'ease-in-out', duration: 500 })
+	const isExistCompletedTasks = todos.filter(todo => todo.completed === false)
+	const isExistDoneTasks = todos.filter(todo => todo.completed === true)
 	useEffect(() => {
-		if (status === "idle") {
-			dispatch(fetchTodos());
+		if (status === 'idle') {
+			dispatch(fetchTodos())
 		}
-	}, [dispatch, status]);
+	}, [dispatch, status])
 
-	if (status === "loading") {
-		return status === "loading" && <p>Loading...</p>;
+	if (status === 'loading') {
+		return status === 'loading' && <p>Loading...</p>
 	}
-	if (status === "failed") {
-		return status === "failed" && <p>Error: {error}</p>;
+	if (status === 'failed') {
+		return status === 'failed' && <p>Error: {error}</p>
 	}
 	return (
 		<div className={st.main}>
 			<TaskInput />
-			<div>
-				{todos.map(
-					(todo) =>
-						todo.completed === false && (
-							<div key={todo.id}>
+			<div className={st.main__tasks} ref={parent}>
+				{isExistCompletedTasks.length > 0 && (
+					<div className={st.main__title}>
+						<Typography variant='h6'>Completed Tasks</Typography>
+						<Typography variant='h6' component={'span'}>
+							- {isExistCompletedTasks.length}
+						</Typography>
+					</div>
+				)}
+				<div>
+					{todos.map(
+						todo =>
+							todo.completed === false && (
 								<Task
+									key={todo.id}
 									title={todo.title}
 									id={todo.id}
 									userId={todo.userId}
 									completed={todo.completed}
 								/>
-							</div>
-						),
-				)}
+							)
+					)}
+				</div>
 			</div>
-			{todos.map(
-				(todo) =>
-					todo.completed === true && <div key={todo.id}>{todo.title}</div>,
-			)}
+			<div className={st.main__tasks} ref={parent}>
+				{isExistDoneTasks.length > 0 && (
+					<div className={st.main__title}>
+						<Typography variant='h6'>Done Tasks</Typography>
+						<Typography variant='h6' component={'span'}>
+							- {isExistDoneTasks.length}
+						</Typography>
+					</div>
+				)}
+				<div>
+					{todos.map(
+						todo =>
+							todo.completed === true && (
+								<Task
+									key={todo.id}
+									title={todo.title}
+									id={todo.id}
+									userId={todo.userId}
+									completed={todo.completed}
+								/>
+							)
+					)}
+				</div>
+			</div>
 		</div>
-	);
-};
+	)
+}
 
-export default MainPage;
+export default MainPage
